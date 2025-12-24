dc8d717b6f44f0ad44a7e0473e2dfe1d6c82ff58
Akase Haruka <lightsing@users.noreply.github.com>
2025-12-24 12:41:55 +0800

    feat: add submit_digest bench (#10)
    * add bench
    
    * apply review

--- crates/calendar/src/main.rs
+++ crates/calendar/src/main.rs
@@ -1,11 +1,6 @@
-#![feature(allocator_api)]
 //! Calendar server
 
-#[macro_use]
-extern crate tracing;
-
 use alloy_primitives::b256;
-use alloy_signer::k256::ecdsa::SigningKey;
 use alloy_signer_local::LocalSigner;
 use axum::{
     Router,
@@ -13,21 +8,13 @@ use axum::{
     routing::{get, post},
 };
 use std::sync::Arc;
-
-mod routes;
-pub mod time;
-
-/// Application state shared across handlers.
-#[derive(Debug)]
-pub struct AppState {
-    signer: LocalSigner<SigningKey>,
-}
+use uts_calendar::{AppState, routes, shutdown_signal, time};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt::init();
 
-    tokio::spawn(time::updater());
+    tokio::spawn(time::async_updater());
 
     let signer = LocalSigner::from_bytes(&b256!(
         "9ba9926331eb5f4995f1e358f57ba1faab8b005b51928d2fdaea16e69a6ad225"
@@ -52,30 +39,4 @@ async fn main() -> eyre::Result<()> {
         .await?;
 
     Ok(())
-}
-
-async fn shutdown_signal() {
-    use tokio::signal;
-
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
 }
