df4b56e69c876bb3bfa6ff6fabd288348d00e831
lightsing <light.tsing@gmail.com>
2026-03-12 01:37:00 +0800

    feat(sdk-go): implement attestation dispatcher for verification

---
+++ packages/sdk-go/attestation/verify.go
@@ -0,0 +1,28 @@
+package attestation
+
+import (
+	"context"
+
+	"github.com/uts-dot/sdk-go/types"
+)
+
+func Verify(ctx context.Context, btcRPC BitcoinRPCClient, easClient EASClient, digest []byte, att types.Attestation) *types.AttestationStatus {
+	switch a := att.(type) {
+	case *types.PendingAttestation:
+		return &types.AttestationStatus{
+			Attestation: att,
+			Status:      types.StatusPending,
+		}
+	case *types.BitcoinAttestation:
+		return VerifyBitcoin(ctx, btcRPC, digest, a)
+	case *types.EASAttestation:
+		return VerifyEASAttestation(ctx, easClient, digest, a)
+	case *types.EASTimestamped:
+		return VerifyEASTimestamped(ctx, easClient, digest, a)
+	default:
+		return &types.AttestationStatus{
+			Attestation: att,
+			Status:      types.StatusUnknown,
+		}
+	}
+}
